5390f37b585e2fc6e9baea1b2b8f7f5b10c8cb55
balraaz15 <balraaz11@gmail.com>
2020-02-22 01:12:05 +0545

    imported search model in main controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,8 @@
 // Spoonacular API Key : f17a5a50930a46ee878e7608b9f48fe8
 // URL : [GET] https://api.spoonacular.com/recipes/search
 
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query) {
-	const apiKey = 'f17a5a50930a46ee878e7608b9f48fe8';
-	const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-	try {
-		// const result = await axios.get(`${corsProxy}https://api.spoonacular.com/recipes/search?apiKey=${apiKey}&query=${query}&number=2`);
-		// console.log(result.data.results);
-	} catch (error) {
-		alert(error);
-	}
-}
+const search = new Search('pizza');
 
-getResults('cheese');
+search.getRecipes();
