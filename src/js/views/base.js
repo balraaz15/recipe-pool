d8b48135d8e4b0200d63d6278b9bb28cc4491da0
balraaz15 <balraaz11@gmail.com>
2020-03-21 23:59:42 +0545

    added selectors

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,9 +3,12 @@
  */
 
 export const elements = {
+	title: document.querySelector('.title'),
 	recipeForm: document.querySelector('.search-recipe'),
 	recipeInput: document.querySelector('.search-box'),
 	searchLoader: document.querySelector('.loader-search'),
 	descLoader: document.querySelector('.loader-desc'),
 	resultList: document.querySelector('.results-list'),
+	resultItem: document.querySelector('.results-list .result-item'),
+	resultItemTitle: document.querySelector('.results-list .result-item h6'),
 };
