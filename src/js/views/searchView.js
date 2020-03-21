774789a2281c8851b8e352cb2da38a6a8d78af1e
balraaz15 <balraaz11@gmail.com>
2020-03-22 00:00:11 +0545

    updated search view to use anchor tags

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,16 +10,15 @@ export const renderResults = (recipes) => {
 	let recipeItem;
 	recipes.map(recipe => {
 		recipeItem = document.createElement('li');
-		recipeItem.className = 'result-item row';
-		recipeItem.setAttribute('data-recipe-id', recipe.id);
-		recipeItem.innerHTML = `<div class="col-md-4">
+		recipeItem.className = 'result-item';
+		recipeItem.innerHTML = `<a class="row" href="#${recipe.id}"><div class="col-md-4">
 						<img src="https://spoonacular.com/recipeImages/${recipe.id}-90x90.jpg" alt="${recipe.title}" crossorigin="anonymous" />
 					</div>
 					<div class="col-md-8">
 						<h6>${recipe.title}</h6>
-						<p>Servings: ${recipe.servings}</p>
-						<p>${recipe.readyInMinutes} mins.</p>
-					</div>`;
+						<p><i class="fas fa-concierge-bell"></i> ${recipe.servings} person(s)</p>
+						<p><i class="fas fa-stopwatch"></i> ${recipe.readyInMinutes} mins.</p>
+					</div></a>`;
 
 		elements.resultList.insertAdjacentElement('beforeend', recipeItem);
 	});
