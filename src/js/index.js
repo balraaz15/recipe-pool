24796922e3a134fc32eb1ecce79c46391ecfe2b5
balraaz15 <balraaz11@gmail.com>
2020-03-21 23:59:03 +0545

    added Recipe Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,5 @@
-// Spoonacular API Key : f17a5a50930a46ee878e7608b9f48fe8
-// URL : [GET] https://api.spoonacular.com/recipes/search
-
 import Search from '../js/models/Search';
+import Recipe from '../js/models/Recipe';
 import { elements } from './views/base';
 import * as searchView from './views/searchView';
 
@@ -14,14 +12,17 @@ import * as searchView from './views/searchView';
  */
 const state = {};
 
+/** 
+ * SEARCH CONTROLLER 
+ */
 const searchRecipe = async () => {
 	const query = searchView.searchValue();
 	if (query) {
 		state.search = new Search(query);
 		elements.searchLoader.style.display = 'block';
 		searchView.clearResult();
-		state.recipe = await state.search.getRecipes();
-		searchView.renderResults(state.recipe);
+		state.recipes = await state.search.getRecipes();
+		searchView.renderResults(state.recipes);
 		elements.searchLoader.style.display = 'none';
 	}
 	console.log(state);
@@ -31,3 +32,26 @@ elements.recipeForm.addEventListener('submit', e => {
 	e.preventDefault();
 	searchRecipe();
 });
+
+/** 
+ * RECIPE CONTROLLER 
+ */
+const controlRecipe = async () => {
+	const id = window.location.hash.replace('#', '');
+	console.log(`Id: ${id}`);
+
+	if (id) {
+		elements.descLoader.style.display = 'block';
+		state.recipe = new Recipe(id);
+		state.recipeData = await state.recipe.getRecipe();
+		elements.descLoader.style.display = 'none';
+	}
+	console.log(state);
+}
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+/**
+ * Page Title
+*/
+elements.title.setAttribute('href', window.origin);
